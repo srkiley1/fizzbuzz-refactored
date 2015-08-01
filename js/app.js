86da4b24597d15a774d5f2793bc49057abf55a94
srkiley1 <srk2@att.net>
2015-08-01 13:28:54 -0700

    used parseInt

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
 	function fizzbuzz () {
 	 
-		for (number=1; number<=20; number++){
+		for (i=1; i<=20; i++){
 	
 			if (number % 15 === 0){
 				$("#append2").append("<li>FizzBuzz</li>");
@@ -10,16 +10,16 @@ $(document).ready( function(){
 				$("#append2").append("<li>Fizz</li>");
 			}
 			
-			else if(number % 5=== 0){
+			else if (number % 5=== 0){
 				$("#append2").append("<li>Buzz</li>");
 			}
-			else {
-				$("#append2").append("<li>"+number+"</li>");
-			}
+			//else {
+				//$("#append2").append("<li>"+number+"</li>");
+			//}
 		}
 	}
 	var number =prompt("please enter a number between 1 and 20")
-	number
-	document.write(number)
+	number =parseInt($("number").attr("#append2"));
 	fizzbuzz ()
+	$("#append2").append("<li>" + number + "</li>");
 });
