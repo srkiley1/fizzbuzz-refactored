f6764d058d31a601a93d538bdfafe47dccc6b51c
srkiley1 <srk2@att.net>
2015-08-03 21:49:36 -0700

    js files changed per session.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 $(document).ready( function(){
 	function fizzbuzz () {
 	 
-		for (i=1; i<=20; i++){
+		for (i=1; i<=number; i++){
 	
-			if (number % 15 === 0){
+			if (i % 15 === 0){
 				$("#append2").append("<li>FizzBuzz</li>");
 			}
-			else if (number % 3 === 0){
+			else if (i % 3 === 0){
 				$("#append2").append("<li>Fizz</li>");
 			}
 			
-			else if (number % 5=== 0){
+			else if (i % 5=== 0){
 				$("#append2").append("<li>Buzz</li>");
 			}
-			//else {
-				//$("#append2").append("<li>"+number+"</li>");
-			//}
+			else {
+				$("#append2").append("<li>"+i+"</li>");
+			}
 		}
 	}
 	var number =prompt("please enter a number between 1 and 20")
-	number =parseInt($("number").attr("#append2"));
+	//number =parseInt($("number").attr("#append2"));
 	fizzbuzz ()
-	$("#append2").append("<li>" + number + "</li>");
+	//$("#append2").append("<li>" + number + "</li>");
 });
